bb0611f4b4a58e0c62982be3257673b3c90b77c2
Jonatas Camargo <jonatascamargo28@gmail.com>
2023-09-25 15:39:03 -0300

    Initial commit

---
+++ criando_bot.py
@@ -0,0 +1,23 @@
+import pyautogui
+#pausa de 2seg entre os comandos, para não dar erro
+pyautogui.PAUSE = 2
+
+#Abrir o sistema, ferramenta ou programa
+pyautogui.press("win") #abrir o pesquisa windows
+pyautogui.write("Instagram") #escrever no pesquisa
+pyautogui.press("backspace") #macete para reconhecer o que foi digitado
+pyautogui.press('enter') #abrir o programa
+
+#preencher o login
+import time
+time.sleep(2)
+
+pyautogui.click(x=925, y=293)
+pyautogui.write("jubelasco21@gmail.com")
+
+#preencher a senha
+pyautogui.click(x=881, y=358)
+pyautogui.write("soueubb1234!")
+
+#clicar em fazer login
+pyautogui.click(x=889, y=417)
---
+++ pegar_pixel_login.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+import time
+
+time.sleep(3)
+pixel = pyautogui.position() #pegar posição do seu mouse
+print(pixel)
